cd558fb4ff6e551b4527c86018a63171c9ef8c8d
Song Guanxian <103612959+songguanxian@users.noreply.github.com>
2025-03-24 10:43:12 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,23 @@
+from PIL import Image
+from unet import Unet
+
+if __name__ == "__main__":
+    unet = Unet()
+    mode            = "dir_predict"
+    name_classes    = ["fracture"]
+    dir_origin_path = "img_in/"
+    dir_save_path   = "img_out/"
+
+    if mode == "dir_predict":
+        import os
+        from tqdm import tqdm
+
+        img_names = os.listdir(dir_origin_path)
+        for img_name in tqdm(img_names):
+            if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                image_path  = os.path.join(dir_origin_path, img_name)
+                image       = Image.open(image_path)
+                r_image     = unet.detect_image(image)
+                if not os.path.exists(dir_save_path):
+                    os.makedirs(dir_save_path)
+                r_image.save(os.path.join(dir_save_path, img_name))
